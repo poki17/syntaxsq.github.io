558c924084562c776f535cfc499c466f9c9ab460
poki17 <98232044+poki17@users.noreply.github.com>
2024-11-30 01:35:19 +0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,33 @@
+const modal = document.getElementById("modal");
+const openModal = document.getElementById("openModal");
+const closeModal = document.getElementById("closeModal");
+
+openModal.addEventListener("click", () => {
+    modal.style.display = "flex";
+});
+closeModal.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
+const modal1 = document.getElementById("modal1");
+const openModal1 = document.getElementById("openModal1");
+const closeModal1 = document.getElementById("closeModal1");
+
+openModal1.addEventListener("click", () => {
+    modal1.style.display = "flex";
+});
+closeModal1.addEventListener("click", () => {
+    modal1.style.display = "none";
+});
+
+
+const modal2 = document.getElementById("modal2");
+const openModal2 = document.getElementById("openModal2");
+const closeModal2 = document.getElementById("closeModal2");
+
+openModal2.addEventListener("click", () => {
+    modal2.style.display = "flex";
+});
+closeModal2.addEventListener("click", () => {
+    modal2.style.display = "none";
+});
